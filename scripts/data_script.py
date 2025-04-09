0d5e6c99b7368b8c57c7637185b588b6d4552753
JohnK <johnkenn777@gmail.com>
2025-04-09 18:22:13 -0400

    Updated data_script.py with version 1.7

--- scripts/data_script.py
+++ scripts/data_script.py
@@ -1,4 +1,4 @@
 print("Starting my data journey!")
-print("Version:", 1.0.07)
+print("Version:", 1.7)
 print("Publish pipeline artfifact to Artifact Feed!")
 print("Push feature branch to Azure Repo, created pipeline artifact, pushed to Artifact Feed!")
